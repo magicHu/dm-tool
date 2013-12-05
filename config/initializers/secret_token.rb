0211f648ee097708cab519d5beb5ac2b10224640
magic <huronghai2008@gmail.com>
2013-12-05 15:22:33 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DmTool::Application.config.secret_key_base = 'b8c6c43b1749aad3b30c628fd491493bc5ca4578820b60ffb590628e1589b9c82068d4c7fcd3c87517da0e7fed10684f539bf16431db69e6a50fd63b7c07366b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DmTool::Application.config.session_store :cookie_store, key: '_dm-tool_session'
