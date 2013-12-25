84a297d5ff3b02ebec07ec09fdb6ca7505722825
magic <huronghai2008@gmail.com>
2013-12-25 16:14:29 +0800

    add unicorn config.

---
+++ config/unicorn.rb
@@ -0,0 +1,13 @@
+# unicorn_rails -c /home/magic/workspace/dm-tool/config/unicorn.rb -E production -D
+
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+worker_processes 2
+
+#project_path = "/home/magic/workspace/dm-tool"
+#stderr_path "#{project_path}/log/unicorn.log"
+#stdout_path "#{project_path}/log/unicorn.log"
+
+listen = 3000
+
+timeout 30
