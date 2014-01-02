2f7b48771bf2843b2dcf11bd34d4a8916862d069
magic <huronghai2008@gmail.com>
2014-01-02 18:01:19 +0800

    add unicorn init shell script.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,11 +4,49 @@ rails_env = ENV['RAILS_ENV'] || 'production'
 
 worker_processes 2
 
-project_path = "/home/magic/workspace/dm-tool"
-stderr_path "#{project_path}/log/unicorn.log"
-stdout_path "#{project_path}/log/unicorn.log"
+app_root = File.expand_path("../..", __FILE__)
+working_directory app_root
+
+stderr_path "#{app_root}/log/unicorn.log"
+stdout_path "#{app_root}/log/unicorn.log"
+
+# App PID
+pid "#{app_root}/tmp/pids/unicorn.pid"
 
 listen "/tmp/unicorn.dm-tool.sock", :backlog => 64
 listen 3000, :tcp_nopush => true
 
 timeout 30
+
+# Force the bundler gemfile environment variable to
+# reference the Сapistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(app_root, 'Gemfile')
+end
+
+
+before_fork do |server, worker|
+  # 参考 http://unicorn.bogomips.org/SIGNALS.html
+  # 使用USR2信号，以及在进程完成后用QUIT信号来实现无缝重启
+  old_pid = app_root + '/tmp/pids/unicorn.pid.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  # 禁止GC，配合后续的OOB，来减少请求的执行时间
+  GC.disable
+  # the following is *required* for Rails + "preload_app true",
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
