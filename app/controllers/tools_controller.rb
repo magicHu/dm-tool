ca017cdaf292f55afd7ad679f36d2c580a2d9b51
magic <huronghai2008@gmail.com>
2014-01-13 17:22:29 +0800

    fix download hdfs bug.

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -6,7 +6,15 @@ class ToolsController < ApplicationController
     hdfs_path = params[:hdfs_path]
     local_path = params[:lcoal_path]
 
-    system("hadoop fs -getmerge #{hdfs_path} #{local_path}")
+    command = %(#!/bin/bash
+export JAVA_HOME=/opt/jdk1.6.0_31
+export PATH=$JAVA_HOME/bin:$PATH
+export CLASSPATH=.:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar:$CLASSPATH
+export HADOOP_MAPRED_HOME=/usr/lib/hadoop-mapreduce
+
+)
+    command << "hadoop fs -getmerge #{hdfs_path} #{local_path}"
+    system("#{command}")
 
     respond_to do |format|
       format.html { redirect_to action: 'index', notice: "download #{hdfs_path} to #{local_path} success." }
