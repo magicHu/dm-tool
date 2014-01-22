0352f0dda422a42beb60a9be1d25639f5a7dee91
magic <huronghai2008@gmail.com>
2014-01-22 11:01:06 +0800

    fix hdfs getmerge bug.

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -6,18 +6,8 @@ class ToolsController < ApplicationController
     hdfs_path = params[:hdfs_path]
     local_path = params[:local_path]
 
-    command = %(#!/bin/bash
-export JAVA_HOME=/opt/jdk1.6.0_31
-export PATH=$JAVA_HOME/bin:$PATH
-export CLASSPATH=.:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dt.jar:$CLASSPATH
-export HADOOP_MAPRED_HOME=/usr/lib/hadoop-mapreduce
-
-)
-    command = "hadoop fs -getmerge #{hdfs_path} #{local_path}"
-    binding.pry
+    command = "hadoop fs -getmerge #{hdfs_path} #{local_path}"    
     system("#{command}", :out =>["#{pig_log_dir}/hdfs.log", 'w'])
-
-    binding.pry
 
     respond_to do |format|
       format.html { redirect_to action: 'index', notice: "download #{hdfs_path} to #{local_path} success." }
